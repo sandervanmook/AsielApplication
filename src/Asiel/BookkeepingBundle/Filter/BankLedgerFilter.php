d325971b7560cfabb9987569c4c50ab4bcebb889
Sander van Mook <sandervanmook84@gmail.com>
2017-02-13 14:26:10 +0100

    bank ledger added to bookkeepingbundle

---
+++ src/Asiel/BookkeepingBundle/Filter/BankLedgerFilter.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Asiel\BookkeepingBundle\Filter;
+
+
+class BankLedgerFilter
+{
+    private $allTransactions;
+    private $searchArray;
+    private $filterResult;
+
+    public function __construct($allTransactions, $searchArray)
+    {
+        $this->allTransactions = $allTransactions;
+        $this->searchArray = $searchArray;
+    }
+
+    public function getFilterResult()
+    {
+        return $this->filterResult;
+    }
+
+    public function filter()
+    {
+        $this->filterDate();
+    }
+
+    /**
+     * First filter, use allActions
+     */
+    private function filterDate()
+    {
+        $result = [];
+        $dateStart = $this->searchArray['datestart']->getTimestamp();
+        $dateEnd = $this->searchArray['dateend']->getTimestamp();
+
+        if (!empty($this->allTransactions)) {
+            foreach ($this->allTransactions as $transaction) {
+                $registerDate = $transaction->getDate()->getTimestamp();
+                if (($registerDate >= $dateStart) &&
+                    ($registerDate <= $dateEnd)
+                ) {
+                    $result[] = $transaction;
+                }
+            }
+            $this->filterResult = $result;
+        }
+    }
+}
