5bf937251964efae35b01d68521fed8bfd402c58
Sander van Mook <sandervanmook84@gmail.com>
2017-02-13 14:56:41 +0100

    add invoice ledger

--- src/Asiel/BookkeepingBundle/Controller/InvoiceLedgerController.php
+++ src/Asiel/BookkeepingBundle/Controller/InvoiceLedgerController.php
@@ -4,13 +4,13 @@
 namespace Asiel\BookkeepingBundle\Controller;
 
 
-use Asiel\BookkeepingBundle\Filter\BankLedgerFilter;
-use Asiel\BookkeepingBundle\Form\BankLedgerSearchType;
+use Asiel\BookkeepingBundle\Filter\InvoiceLedgerFilter;
+use Asiel\BookkeepingBundle\Form\InvoiceLedgerSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class BankLedgerController extends Controller
+class InvoiceLedgerController extends Controller
 {
     /**
      * @param Request $request
@@ -18,7 +18,7 @@ class BankLedgerController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $form = $this->createForm(BankLedgerSearchType::class);
+        $form = $this->createForm(InvoiceLedgerSearchType::class);
 
         $form->handleRequest($request);
         if ($form->isValid() && $form->isSubmitted()) {
@@ -26,18 +26,18 @@ class BankLedgerController extends Controller
             $searchArray['dateend'] = $form->get('dateend')->getData();
             $searchArray['showall'] = $form->get('showall')->getData();
 
-            $allBankTransactions = $this->getDoctrine()->getRepository('BookkeepingBundle:Transaction')->findBy(['paymentType' => 'Bank']);
+            $allInvoiceTransactions = $this->getDoctrine()->getRepository('BookkeepingBundle:Transaction')->findBy(['paymentType' => 'Invoice']);
 
-            $filter = new BankLedgerFilter($allBankTransactions, $searchArray);
+            $filter = new InvoiceLedgerFilter($allInvoiceTransactions, $searchArray);
             $filter->filter();
 
             if ($searchArray['showall']) {
-                $result = $allBankTransactions;
+                $result = $allInvoiceTransactions;
             } else {
                 $result = $filter->getFilterResult();
             }
 
-            return $this->render('@Bookkeeping/Backend/BankLedger/index.html.twig', [
+            return $this->render('@Bookkeeping/Backend/InvoiceLedger/index.html.twig', [
                 'form' => $form->createView(),
                 'datestart' => $searchArray['datestart']->format('d-m-Y'),
                 'dateend' => $searchArray['dateend']->format('d-m-Y'),
@@ -45,7 +45,7 @@ class BankLedgerController extends Controller
             ]);
         }
 
-        return $this->render('@Bookkeeping/Backend/BankLedger/index.html.twig', [
+        return $this->render('@Bookkeeping/Backend/InvoiceLedger/index.html.twig', [
             'form' => $form->createView(),
         ]);
     }
