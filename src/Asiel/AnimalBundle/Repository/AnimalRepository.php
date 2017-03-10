9fb553036c02d3dde968125a392dfdff1b7fdda4
Sander van Mook <sandervanmook84@gmail.com>
2017-03-10 15:52:50 +0100

    fix

--- src/Asiel/AnimalBundle/Repository/AnimalRepository.php
+++ src/Asiel/AnimalBundle/Repository/AnimalRepository.php
@@ -137,7 +137,6 @@ class AnimalRepository extends EntityRepository
         $qb = $this->createQueryBuilder('a');
         $qb->leftJoin('a.status', 's');
         $qb->where('s.onsiteLocation = true' );
-        $qb->where('s.offsiteLocation = false' );
         $qb->andWhere('s.archived = false');
 
         return $qb->getQuery()->getResult();
@@ -148,7 +147,6 @@ class AnimalRepository extends EntityRepository
         $qb = $this->createQueryBuilder('a');
         $qb->leftJoin('a.status', 's');
         $qb->where('s.offsiteLocation = true' );
-        $qb->where('s.onsiteLocation = false' );
         $qb->andWhere('s.archived = false');
 
         return $qb->getQuery()->getResult();
