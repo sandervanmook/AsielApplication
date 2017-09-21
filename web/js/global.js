c588f0ef4ac9b6078222300799a789e9605ab9cc
Sander van Mook <sandervanmook84@gmail.com>
2017-09-21 10:48:25 +0200

    cleanup

--- web/js/global.js
+++ web/js/global.js
@@ -4,8 +4,4 @@ $(document).ready(function(){
         parent.history.back();
         return false;
     });
-    // Let the reset button on the animal search page go back to it's original state
-    $('.btn-reset-customer-index').click(function(){
-        window.location.href = $(location).attr('origin')+'/backend/customer';
-    });
 });
